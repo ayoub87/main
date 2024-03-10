b584678f397160398a8088f6c5d9271aa0b2cdcb
ayoub87 <91567853+ayoub87@users.noreply.github.com>
2024-03-10 23:43:54 +0100

    script.js

--- script.js
+++ script.js
@@ -1,120 +1,44 @@
-// collect DOMs
-const display = document.querySelector('.display')
-const controllerWrapper = document.querySelector('.controllers')
+const textarea = document.querySelector("textarea"),
+voiceList = document.querySelector("select"),
+speechBtn = document.querySelector("button");
 
-const State = ['Initial', 'Record', 'Download']
-let stateIndex = 0
-let mediaRecorder, chunks = [], audioURL = ''
+let synth = window.speechSynthesis,
+isSpeaking = false;
 
-// mediaRecorder setup for audio
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
-    console.log('mediaDevices supported..')
-
-    navigator.mediaDevices.getUserMedia({
-        audio: true
-    }).then(stream => {
-        mediaRecorder = new MediaRecorder(stream)
-
-        mediaRecorder.ondataavailable = (e) => {
-            chunks.push(e.data)
-        }
-
-        mediaRecorder.onstop = () => {
-            const blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'})
-            chunks = []
-            audioURL = window.URL.createObjectURL(blob)
-            document.querySelector('audio').src = audioURL
-
-        }
-    }).catch(error => {
-        console.log('Following error has occured : ',error)
-    })
-}else{
-    stateIndex = ''
-    application(stateIndex)
-}
-
-const clearDisplay = () => {
-    display.textContent = ''
-}
-
-const clearControls = () => {
-    controllerWrapper.textContent = ''
-}
-
-const record = () => {
-    stateIndex = 1
-    mediaRecorder.start()
-    application(stateIndex)
-}
-
-const stopRecording = () => {
-    stateIndex = 2
-    mediaRecorder.stop()
-    application(stateIndex)
-}
-
-const downloadAudio = () => {
-    const downloadLink = document.createElement('a')
-    downloadLink.href = audioURL
-    downloadLink.setAttribute('download', 'audio')
-    downloadLink.click()
-}
-
-const addButton = (id, funString, text) => {
-    const btn = document.createElement('button')
-    btn.id = id
-    btn.setAttribute('onclick', funString)
-    btn.textContent = text
-    controllerWrapper.append(btn)
-}
-
-const addMessage = (text) => {
-    const msg = document.createElement('p')
-    msg.textContent = text
-    display.append(msg)
-}
-
-const addAudio = () => {
-    const audio = document.createElement('audio')
-    audio.controls = true
-    audio.src = audioURL
-    display.append(audio)
-}
-
-const application = (index) => {
-    switch (State[index]) {
-        case 'Initial':
-            clearDisplay()
-            clearControls()
-
-            addButton('record', 'record()', 'Start Recording')
-            break;
-
-        case 'Record':
-            clearDisplay()
-            clearControls()
-
-            addMessage('Recording...')
-            addButton('stop', 'stopRecording()', 'Stop Recording')
-            break
-
-        case 'Download':
-            clearControls()
-            clearDisplay()
-
-            addAudio()
-            addButton('record', 'record()', 'Record Again')
-            break
-
-        default:
-            clearControls()
-            clearDisplay()
-
-            addMessage('Your browser does not support mediaDevices')
-            break;
+function voices(){
+    voiceList.innerHTML = ""; // Clearing previous options
+    for(let voice of synth.getVoices()){
+        let selected = voice.name === "Google US English" ? "selected" : "";
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+        voiceList.insertAdjacentHTML("beforeend", option);
     }
-
 }
 
-application(stateIndex)
+window.speechSynthesis.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text){
+    let utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = synth.getVoices().find(voice => voice.name === voiceList.value);
+    synth.speak(utterance);
+}
+
+speechBtn.addEventListener("click", e =>{
+    e.preventDefault();
+    if(textarea.value !== ""){
+        if(!synth.speaking){
+            textToSpeech(textarea.value);
+            isSpeaking = true;
+            speechBtn.innerText = "Pause Speech";
+        } else {
+            if(isSpeaking){
+                synth.pause();
+                isSpeaking = false;
+                speechBtn.innerText = "Resume Speech";
+            } else {
+                synth.resume();
+                isSpeaking = true;
+                speechBtn.innerText = "Pause Speech";
+            }
+        }
+    }
+});
